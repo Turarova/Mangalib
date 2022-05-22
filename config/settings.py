031664b878af1d5e2e9e83b9e2a088ab3f6e8806
Aigerim <maviboncuaika@gmail.com>
2022-05-22 16:15:53 +0600

    DeployH1

--- config/settings.py
+++ config/settings.py
@@ -165,7 +165,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+EMAIL_HOST_USER = 'maviboncuaika@gmail.com'
+EMAIL_HOST_PASSWORD = 'Turarova_A202277212'
 
 
