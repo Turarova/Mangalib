2a1beaa3d4e210cab23cb60b3982a2aa7b5fe6dd
Aigerim <maviboncuaika@gmail.com>
2022-06-11 15:11:19 +0600

    heroku

--- config/settings.py
+++ config/settings.py
@@ -175,8 +175,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'maviboncuaika@gmail.com'
-EMAIL_HOST_PASSWORD = 'Turarova_A202277212'
+# EMAIL_HOST_USER = 'maviboncuaika@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Turarova_A202277212'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
--- manga/permissions.py
+++ manga/permissions.py
@@ -4,3 +4,5 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsCommentAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and request.user == obj.user
+
+
